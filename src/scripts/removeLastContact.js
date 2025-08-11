5b431cf1aec9d4455f7777079a238687dce6a78e
Vlad <vlad.kuzmenko.dev@gmail.com>
2025-08-11 20:43:28 +0300

    add first homework

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (!contacts.length) {
+    console.log("You don't have contacts");
+    return;
+  }
+  contacts.pop();
+  await writeContacts(contacts);
+};
 
 removeLastContact();
